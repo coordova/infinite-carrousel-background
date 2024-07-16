c99c5cf3de8ecc1244ad88b5d179238b2a221c73
coordova <grcordova@gmail.com>
2024-07-16 01:40:07 -0400

    Infinite Carousels Background - javascript

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// Wait for content on the page to load
+window.addEventListener("DOMContentLoaded", () => {
+	// Get all carousels on the page
+	const carousels = document.querySelectorAll(".infinite-carousel");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+	// Set direction for each carousel (column)
+	const direction = ["normal", "reverse", "normal", "reverse"];
 
-setupCounter(document.querySelector('#counter'))
+	// Set speed for each carousel
+	const speed = [35, 50, 30, 42];
+
+	// Loop through each carousel
+	carousels.forEach((carousel, index) => {
+		// Get the inner div of each carousel
+		const carouselInner = carousel.querySelector(".infinite-carousel div");
+		// Get the inner content (images) from the carousel
+		const carouselContent = Array.from(carouselInner.children);
+		// Loop through each image in the carousel
+		carouselContent.forEach((item) => {
+			// duplicate each item
+			const duplicateItem = item.cloneNode(true);
+			// append the duplicate item to the carousel
+			carouselInner.appendChild(duplicateItem);
+		});
+
+		// Add animation to the carousel
+		carouselInner.style.animation = `move ${speed[index]}s linear infinite ${direction[index]}`;
+
+		/* If we add it it the CSS and not here, the animation will be applied before the item are duplicated, and it will not work properly */
+	});
+});
